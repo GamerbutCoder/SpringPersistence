7d2ed117902bdaffcabfe1c798a8daeeed5aab83
nagadeepakbhushan <nagadeepak000@gmail.com>
2021-02-23 18:18:21 +0530

    redis-added

--- src/main/java/com/quinbay/SpringPersistence/entity/Employee.java
+++ src/main/java/com/quinbay/SpringPersistence/entity/Employee.java
@@ -2,14 +2,14 @@ package com.quinbay.SpringPersistence.entity;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.MongoId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
 
-@Document
+@RedisHash(value = "employee")
 @Getter
 @Setter
 public class Employee {
-    @MongoId
+    @Id
     private Long id;
 
     private String name;
